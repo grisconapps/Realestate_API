4e9e1a7e0a6a88f8059382c4476c8ef2d3797267
Sasidhar Chintapalli <sasi.ch85@gmail.com>
2013-11-11 20:45:00 +0530

    property details issue fix

--- propertyDetails.class.php
+++ propertyDetails.class.php
@@ -6,7 +6,7 @@ class propertyDetails
 
 	public 	$query ;
 	public $dbConn;
-
+	public $insertNew;
 
 	public function propertyDetails($type, $cat)
 	{
@@ -17,106 +17,123 @@ class propertyDetails
 	}
   private function init($type,$cat)
   {
-     switch($type)
-     {
-     case 'list':
+	  $this->insertNew = false;
+	  switch($type)
+	  {
+		  case 'list':
 
-       $this->query = "select * from property";
-       $params = '';
-       if ( $cat == 'filter' )
-       {
-         $params = $this->parseListParams();
-       }
-        $this->query .= "$params;";
-       break;
-     
-     case 'add':
-       $this->query = "insert into property ".$this->parseAddParams();
-       break;
-     case 'edit':
+			  $this->query = "select * from property";
+			  $params = '';
+			  if ( $cat == 'filter' )
+			  {
+				  $params = $this->parseListParams();
+			  }
+			  $this->query .= "$params;";
+			  break;
 
-        $this->query = "update property set "; // Update property with params added
-          $params = $this->parseUpdateParams();
-        $this->query .= "$params;";
-       break;
+		  case 'add':
 
-    default;  
-     }
+			  $this->insertNew = true;
+			  $this->query = "insert into property ".$this->parseAddParams();
+			  break;
+
+		  case 'edit':
+
+			  $this->query = "update property set "; // Update property with params added
+			  $params = $this->parseUpdateParams();
+			  $this->query .= "$params;";
+			  break;
+
+		  default:
+			error_log("propertyDetail.init type = $type");  
+	  }
   }
 
   private function parseListParams()
   {
-    $queryParams = '';
-    foreach ( $_REQUEST as $key=>$value)
-    {
-      if ( $key == 'name' || $key == 'type' || $key == 'cat' || $key == 'clientId' )
-      {
-        continue;
-      }
-      else if ( $queryParams != '' )
-      {
-          $queryParams .= " and "; 
-      }
-    
-      $queryParams .= "$key=$value";
+	  $queryParams = '';
+	  foreach ( $_REQUEST as $key=>$value)
+	  {
+		  if ( $key == 'name' || $key == 'type' || $key == 'cat' || $key == 'clientId' )
+		  {
+			  continue;
+		  }
+		  else if ( $queryParams != '' )
+		  {
+			  $queryParams .= " and "; 
+		  }
 
-    }
+		  $queryParams .= "$key=\"$value\"";
+
+	  }
     return $queryParams;
   }
   private function parseAddParams()
   {
-    $queryKeys = '';
-    $queryValues = '';
+	  $queryKeys = '';
+	  $queryValues = '';
 
-    foreach ( $_REQUEST as $key=>$value)
-    {
-      if ( $key == 'name' || $key == 'type' || $key == 'cat' || $key == 'clientId' )
-        continue;
-      if ( $queryKeys != '' )
-        $queryKeys .= ',';
-      if ( $queryValues != '' )
-        $queryValues .= ',';
+	  foreach ( $_REQUEST as $key=>$value)
+	  {
+		  if ( $key == 'name' || $key == 'type' || $key == 'cat' || $key == 'clientId' )
+			  continue;
+		  if ( $queryKeys != '' )
+			  $queryKeys .= ',';
+		  if ( $queryValues != '' )
+			  $queryValues .= ',';
 
-      $queryKeys .= $key;
-      $queryValues .= "\"$value\"";
+		  $queryKeys .= $key;
+		  $queryValues .= "\"$value\"";
 
-    }
+	  }
 
-    return " ($queryKeys) values ($queryValues);";
+	  return " ($queryKeys) values ($queryValues);";
   }
 
   private function parseUpdateParams()
   {
-      $queryUpdate = '';
-      $queryWhere = '';
-    foreach ( $_REQUEST as $key=>$value)
-    {
+	  $queryUpdate = '';
+	  $queryWhere = '';
+	  foreach ( $_REQUEST as $key=>$value)
+	  {
 
-      if ( $key == 'name' || $key == 'type' || $key == 'cat' || $key == 'clientId' )
-        continue;
+		  if ( $key == 'name' || $key == 'type' || $key == 'cat' || $key == 'clientId' )
+			  continue;
 
-      if ( $key == 'propId')
-      {
-        $queryWhere .= " $key=$value"; 
-         continue;
-      }
+		  if ( $key == 'propId')
+		  {
+			  $queryWhere .= " $key=\"$value\""; 
+			  continue;
+		  }
 
-      if ( $queryUpdate != '' )
-      {
-        $queryUpdate .= ',';
-      }
-  
-      $queryUpdate .= "$key=$value";
+		  if ( $queryUpdate != '' )
+		  {
+			  $queryUpdate .= ',';
+		  }
 
-    }
+		  $queryUpdate .= "$key=\"$value\"";
 
-    return "$quertUpdate where $queryWhere";
+	  }
+
+	  return "$quertUpdate where $queryWhere";
   }
 
   public function executeQuery()
   {
-
+	  
 	  $result = $this->dbConn->prepareQuery($this->query);
+	  if ( mysql_errno() || (!$result && !$this->insertNew))
+	  {
+		  $xml  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		  $xml .= "<error>".mysql_error()."</error>";
+		  $result = $xml;
+	  }
+	  if ( !mysql_errno() && !$result && $this->insertNew)
+	  {
+		  $xml  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		  $xml .= "<results></results>";
+		  $result = $xml;
+	  }			    
 	  return $result;
   }
   public function close()
